0c65d13acb8eadaf618b69bd44fbffcf5f796712
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-04-13 11:51:46 -0700

    init

--- multiversion_test.go
+++ multiversion_test.go
@@ -3,12 +3,11 @@ package multiverson
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
-	consul_v1_13_2_version "gomodv.hashicorp.com/github.com/hashicorp/consul__v1.13.2/version"
-	consul_v1_13_3_version "gomodv.hashicorp.com/github.com/hashicorp/consul__v1.13.3/version"
+	consul_v1_13_2_version "consul__v1.13.2/version"
+	consul_v1_13_3_version "consul__v1.13.3/version"
 )
 
 func TestFoo(t *testing.T) {
-	require.Equal(t, consul_v1_13_3_version.Version, "1.13.3")
-	require.Equal(t, consul_v1_13_2_version.Version, "1.13.2")
+	t.Logf("v1.13.2: %s", consul_v1_13_2_version.GetHumanVersion())
+	t.Logf("v1.13.3: %s", consul_v1_13_3_version.GetHumanVersion())
 }
